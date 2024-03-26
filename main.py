f72a7a0fa3d1436152334a4a843b328f2da406ee
ironbeardx <alejandroyeroval@gmail.com>
2024-03-26 02:52:17 -0400

    adding statistical information storage

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class InventorySimulation:
                 S:int = 20,
                 holding_cost_rate:int = 3, 
                 lead_time:int = 10, 
-                sim_duration:int=1000, 
+                sim_duration:int=200, 
                 initial_balance:int=100,
                 product_value:int = 10,
                 initial_inventory_level:int= 0,
@@ -43,7 +43,35 @@ class InventorySimulation:
         self.client_attention_dist = client_attention_dist # Distribution of customer attention
         self.client_demand_dist = client_demand_dist # Distribution of the amount of the customer demand
 
-    #[x] missing initializing statistical stuff
+        # Variables for storing statistical information
+        self.st_inventory_level_records = [] # Records of the inventory levels for each event
+        self.st_holding_payment_records = [] # Records of the holding costs for each event
+        self.st_lost_sale_records = [] # Records of the lost sales for each event
+        self.st_order_records = [] # Records of the orders for each event
+        self.st_order_payment_records = [] # Records of the ordering costs for each event
+        self.st_balance_records = [] # Records of the balance for each event
+        self.st_earning_records = [] # Records of the earnings for each event
+        self.st_sell_records = [] # Records of the sells for each event
+
+    #[x]
+    def demand_event_snapshot(self, sold_product:int, gain:int, lost_sale:int):
+        # A snapshot of the necesary information on a demand event
+        self.st_earning_records.append((self.time, gain))
+        self.st_sell_records.append((self.time, sold_product))
+        self.st_balance_records.append((self.time, self.balance))# [ ] if Two events happen at the same time it will be a problem
+        self.st_inventory_level_records.append((self.time, self.inventory_level))
+        self.st_lost_sale_records.append((self.time, lost_sale))
+
+    #[x]
+    def order_event_snapshot(self, order:int):
+        # A snapshot of the necesary information on a order event
+        self.st_order_records.append((self.time, order))
+        self.st_order_payment_records.append((self.time, self.ordering_cost_func(order)))
+        self.st_inventory_level_records.append((self.time, self.inventory_level))
+        self.st_balance_records.append((self.time, self.balance))# [ ] if Two events happen at the same time it will be a problem
+
+
+    #[x]
     def initialize_simulation(self):
         self.time = 0
         self.pending_orders = []
@@ -51,7 +79,17 @@ class InventorySimulation:
         self.total_lost_sales = 0
         self.inventory_level = self.initial_inventory_level
 
-    #[x] missing demand randomization information and statistical stuff
+        # Initializing statistical records
+        self.st_inventory_level_records = []
+        self.st_holding_payment_records = []
+        self.st_lost_sale_records = []
+        self.st_order_records = []
+        self.st_balance_records = []
+        self.st_earning_records = []
+        self.st_sell_records = []
+        self.st_order_payment_records = []
+
+    #[x] missing demand randomization information
     def generate_client_arrival(self):
         # Generate customer demand according to a specified distribution
         arrival_time, demand = self.clients_arrival_dist(), self.client_demand_dist()
@@ -59,7 +97,7 @@ class InventorySimulation:
             return None, None
         return self.time + arrival_time, demand
 
-    #[x] missing statistical stuff 
+    #[x]
     def place_order(self):
         # Determine when to place orders based on the (s, S) policy
         if self.inventory_level <= self.s - sum(self.pending_orders):
@@ -72,32 +110,42 @@ class InventorySimulation:
             return self.time + self.lead_time, order_quantity
         return None, None
 
-    #[x] missing statistical stuff
+    #[x]
     def calculate_holding_costs(self, interval_time:int):
         # Calculate holding costs and holding costs incurred during the simulation
         self.balance -= (self.holding_cost_rate * self.inventory_level) * interval_time
+        self.st_holding_payment_records.append((self.time, (self.holding_cost_rate * self.inventory_level) * interval_time))
 
-    #[x] missing statistical stuff
+    #[x]
     def fulfill_demand(self, demand:int):
         # Fulfill customer demand based on available inventory
+        gain = 0
+        lost_sale = 0
         if self.inventory_level >= demand:
             self.inventory_level -= demand
-            self.balance += demand * self.product_value
+            sold_product, gain = demand, demand * self.product_value
+            self.balance += gain
         else:
-            remaining_demand = demand - self.inventory_level
+            lost_sale = demand - self.inventory_level
             self.inventory_level = 0
-            self.total_lost_sales += remaining_demand * self.product_value
-            self.balance += (demand - remaining_demand) * self.product_value
+            self.total_lost_sales += lost_sale * self.product_value
+            sold_product, gain = demand - lost_sale, (demand - lost_sale) * self.product_value
+            self.balance += gain
+        
+        self.demand_event_snapshot(sold_product, gain, lost_sale)
+
         return self.generate_client_arrival()
     
-    #[x] missing statistical stuff
+    #[x]
     def simulate_order_delivery(self, order_amount:int):
         # Simulate order delivery lead times and update inventory levels accordingly
         self.inventory_level += order_amount
         self.balance -= self.ordering_cost_func(order_amount)
         self.pending_orders.remove(order_amount)
 
-    #[x] missing statistical stuff
+        self.order_event_snapshot(order_amount)
+
+    #[x]
     def run_simulation(self):
         # Creating a priority queue for events
         event_queue = []
@@ -117,7 +165,7 @@ class InventorySimulation:
             heapq.heappush(event_queue, (first_order, amount, 'order_arrival'))
         
         # Adding the the final event
-        heapq.heappush(event_queue, (self.sim_duration, None, 'ending_simulation'))
+        heapq.heappush(event_queue, (self.sim_duration + 1, None, 'ending_simulation'))
 
         while event_queue:
             # Get the next event
@@ -141,7 +189,7 @@ class InventorySimulation:
                 # Record the sale [ ]
                 # Generate a provider order if necesary [x]
                 next_client, demand = self.fulfill_demand(event[1])
-                if not next_client:
+                if next_client:
                     heapq.heappush(event_queue, (next_client, demand, 'demand'))
                 next_order, amount = self.place_order()
                 if next_order:
@@ -164,7 +212,7 @@ class InventorySimulation:
                 raise ValueError('Invalid event type')
 
 
-    #[ ] Add statistical information needed in previous methods
+    #[ ]
     def plot_results(self):
         # Plot results of the simulation
         pass
