63993f7004aceb55be3ab348648211158b3dc73d
Leismael Sosa <leismael.sosa17@gmail.com>
2024-03-27 17:06:17 -0400

    Adding comments and formatting the code in the simulation engine

--- clean_code/simulation_engine.py
+++ clean_code/simulation_engine.py
@@ -18,12 +18,12 @@ class InventorySimulation:
         holding_cost_rate: int = 3,  # The pay constant for holding inventory
         holding_pay_time: int = 60,  # 60 minutes, 1 hour. After that time the store must pay for the inventory
         product_value: int = 10,
-        client_arrival_dist: Callable[[],int] = poisson_random_variable(5),
+        client_arrival_dist: Callable[[], int] = poisson_random_variable(5),
         client_demand_dist: Callable = random.randint(1, 10),
-        sim_duration:int = 840 # 14 hours -> 14 * 60min = 840min
+        sim_duration: int = 840,  # 14 hours -> 14 * 60min = 840min
     ):
-        if (s >= S):
-            raise Exception('Problem: s >= S and this should not happen')
+        if s >= S:
+            raise Exception("Problem: s >= S and this should not happen")
         self.s: int = s
         """The minimum amount of units in inventory before ask for supply"""
         self.S: int = S
@@ -36,21 +36,21 @@ class InventorySimulation:
         """This is the time difference between payments for holding the inventory"""
         self.product_value: int = product_value
         # ------ Variables that represents the distributions
-        self.client_arrival_dist: Callable[[],int] = client_arrival_dist
-        self.client_demand_dist: Callable[[],int] = client_demand_dist
+        self.client_arrival_dist: Callable[[], int] = client_arrival_dist
+        self.client_demand_dist: Callable[[], int] = client_demand_dist
         self.registry: Registry = Registry()
         # ------ Now are the variables of the state of the simulation -------
-        self.time:int = 0
+        self.time: int = 0
         """This is the actual simulation time"""
-        self.sim_duration:int = sim_duration
+        self.sim_duration: int = sim_duration
         """The duration of a single simulation"""
         self.actual_balance: int = 0
         """This is the actual balance of the store account in the """
-        self.initial_inventory_level:int = initial_inventory_level
+        self.initial_inventory_level: int = initial_inventory_level
         self.actual_inventory_level: int = initial_inventory_level
         self.pending_order: bool = False
         # -------- Simulation queue --------
-        self.event_queue:list[Event] = []
+        self.event_queue: list[Event] = []
         """The priority queue of events"""
 
     def initialize(self):
@@ -63,14 +63,14 @@ class InventorySimulation:
         self.pending_order = False
         self.registry = Registry()
         self.verify_supply_policy()
-        self.generate_client_arrival_event()
+        self.generate_client_sell_event()
         self.add_simulation_end_event()
 
-    def generate_client_arrival_event(self):
+    def generate_client_sell_event(self):
         """Generate a new Sell Event and push it to the queue"""
-        time:int = self.client_arrival_dist()
-        amount:int = self.client_demand_dist()
-        client_event:SellEvent = SellEvent(self.time + time, amount)
+        time: int = self.client_arrival_dist()
+        amount: int = self.client_demand_dist()
+        client_event: SellEvent = SellEvent(self.time + time, amount)
         self.add_to_event_queue(client_event)
 
     def verify_supply_policy(self):
@@ -84,22 +84,26 @@ class InventorySimulation:
         self.pending_order = True
 
     def add_simulation_end_event(self):
-        end:SimulationEndEvent = SimulationEndEvent(self.sim_duration)
+        """Adds a SimulationEndEvent to the queue with the time equals to the simulation duration"""
+        end: SimulationEndEvent = SimulationEndEvent(self.sim_duration)
         self.add_to_event_queue(end)
 
     def generate_pay_holding_event(self):
-        pay_hold_event:PayHoldingEvent = PayHoldingEvent(self.time + self.holding_pay_time)
+        """Generate a new event for paying the inventory hold"""
+        pay_hold_event: PayHoldingEvent = PayHoldingEvent(
+            self.time + self.holding_pay_time
+        )
         self.add_to_event_queue(pay_hold_event)
 
-    def add_to_event_queue(self, event:Event):
+    def add_to_event_queue(self, event: Event):
         """Adds an event to the simulation queue"""
         heapq.heappush(self.event_queue, event)
 
     def get_next_event_in_queue(self) -> Event:
         """Extracts the next event in the queue"""
-        event:Event = heapq.heappop(self.event_queue)
+        event: Event = heapq.heappop(self.event_queue)
         return event
-    
+
     def process_sell_event(self):
         pass
 
@@ -111,16 +115,13 @@ class InventorySimulation:
 
     def process_simulation_end(self):
         pass
-    
+
     def step(self):
         """Extracts the next event in the Event Queue and process it"""
         event: Event = self.get_next_event_in_queue()
         time = event.time
-        self.time = time # Advance the simulation time to the Event time
-        
+        self.time = time  # Advance the simulation time to the Event time
 
     def run(self):
         """Run the simulation"""
         self.initialize()
-        
-
