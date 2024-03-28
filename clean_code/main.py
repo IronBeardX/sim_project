b143524f054164fba4b58ecedca5fd530e85dd22
ironbeardx <alejandroyeroval@gmail.com>
2024-03-28 00:39:51 -0400

    Adding plots

--- clean_code/main.py
+++ clean_code/main.py
@@ -1,11 +1,20 @@
 from simulation_engine import *
 from sim_stats import FlattenRegistry, SimStatistics
-
-simulation_time:int = 14 * 60 # 14 hours * 60 min = 840
+import matplotlib.pyplot as plt
+simulation_time:int = 24 * 60 # 14 hours * 60 min = 840
 # The simulation time is in minutes
 sim = InventorySimulation(sim_duration=simulation_time, s=10, S= 200)
 sim.run()
 
 sim_statistics = SimStatistics(sim.registry)
 # plotting the seels of the store
-sim_statistics.plot_bar_sell()
+plt.figure(figsize=(12, 20))
+plt.title('Simulation Statistics')
+plt.subplot(3, 1, 1)
+sim_statistics.plot_sells()
+plt.subplot(3, 1, 2)
+sim_statistics.plot_stock()
+plt.subplot(3, 1, 3)
+sim_statistics.plot_holding_costs()
+
+plt.show()
